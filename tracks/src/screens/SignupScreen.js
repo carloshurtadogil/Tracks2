0d09a7b6722d944b8b0c4722c761b0ff415ad80c
Carlos Hurtaado <carloshurtado219@yahoo.com>
2019-09-02 22:19:50 -0700

    Input Styling added to Signup Screen

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -4,10 +4,12 @@ import { Button, Input, Text } from 'react-native-elements';
 import { Spacer } from '../components';
 import { Context as AuthContext } from '../context/AuthContext';
 import { LinearGradient } from 'expo-linear-gradient';
-import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import { Fumi } from 'react-native-textinput-effects';
 
 const SignupScreen = ({ navigation }) => {
-    const { background, container, errorMessage, title } = styles;//Destructure styles for convenience
+    const { background, boxShadow, buttonText, container, errorMessage, title } = styles;//Destructure styles for convenience
     
     //Data held for inputs
     const { state, signup } = useContext( AuthContext ); 
@@ -33,8 +35,15 @@ const SignupScreen = ({ navigation }) => {
                 <Text h3 style={ title }>Tracker</Text>
 
                 <Spacer/>
-                <Input 
-                    label="Email"
+                <Fumi
+                    label={'Email'}
+                    iconClass={FontAwesomeIcon}
+                    iconName={'envelope'}
+                    iconColor={ '#485461' }
+                    iconSize={20}
+                    iconWidth={40}
+                    inputPadding={16}
+                    style={ boxShadow }
                     value={ email }
                     onChangeText={ setEmail }
                     autoCapitalize="none"
@@ -42,8 +51,15 @@ const SignupScreen = ({ navigation }) => {
                 />
 
                 <Spacer/>
-                <Input 
-                    label="Password"
+                <Fumi
+                    label={ 'Password' }
+                    iconClass={ FontAwesomeIcon }
+                    iconName={ 'key' }
+                    iconColor={ '#485461' }
+                    iconSize={20}
+                    iconWidth={40}
+                    inputPadding={16}
+                    style={ boxShadow }
                     value={ password }
                     onChangeText={ setPassword }
                     autoCapitalize="none"
@@ -55,13 +71,14 @@ const SignupScreen = ({ navigation }) => {
                 <Spacer/>
                 <Button 
                     title="Signup"
+                    //onPress={ () => console.log(password) }
                     onPress={ () => signup({ email, password }) }
                 />
 
                 <TouchableOpacity
                     onPress={ () => navigation.navigate('Signin') }
                 >
-                    <Text>Already have an account? Sign in instead</Text>
+                    <Text style={ buttonText } >Already have an account? Sign in instead</Text>
                 </TouchableOpacity>
             </View>
         </LinearGradient>
@@ -77,6 +94,16 @@ SignupScreen.navigationOptions = () => {
 const styles = StyleSheet.create({
     background: {
         flex: 1
+    },
+    boxShadow: {
+        shadowOffset:{  width: 100,  height: 10,  },
+        shadowColor: 'black',
+        shadowOpacity: 1.0
+    },
+    buttonText: {
+        alignSelf: 'center',
+        color: 'white',
+        paddingTop: 15
     },
     container: {
         flex: 1,
