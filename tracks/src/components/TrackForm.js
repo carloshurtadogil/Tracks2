a5f66b5cd23f72bda5473d82bb04bb3aaabdd25c
Carlos Hurtaado <carloshurtado219@yahoo.com>
2019-10-05 21:18:12 -0700

    Connected LocationContext to TrackForm

--- tracks/src/components/TrackForm.js
+++ tracks/src/components/TrackForm.js
@@ -1,15 +1,28 @@
 import React, { useContext } from 'react';
 import { Button, Input } from 'react-native-elements';
 import { Spacer } from './Spacer';
+import { Context as LocationContext } from '../context/LocationContext';
 
 const TrackForm = () => {
+    const {
+        state: { name, recording, locations },
+        startRecording, 
+        stopRecording,
+        changeName
+    } = useContext(LocationContext);
+
+    //console.log(locations.length);
+
     return (
         <>
             <Spacer>
-                <Input placeholder="Enter Name"/>
+                <Input placeholder="Enter Name" onChangeText={ changeName } value={name}/>
             </Spacer>
 
-            <Button title="Start Recording"/>
+            { recording
+                ? <Button title="Stop" onPress={stopRecording}/>
+                : <Button title="Start Recording" onPress={startRecording}/>
+            }
         </>
     );
 }
