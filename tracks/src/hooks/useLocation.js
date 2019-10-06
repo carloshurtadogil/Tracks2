278b7b6db50070a1f8c296cd46e0784e7ae0f48e
Carlos Hurtaado <carloshurtado219@yahoo.com>
2019-10-05 22:21:22 -0700

    Improved useLocation hook to remove bugs

--- tracks/src/hooks/useLocation.js
+++ tracks/src/hooks/useLocation.js
@@ -8,33 +8,39 @@ import {
 //Check the current permissions of the app to ensure that location may be tracked
 export default (shouldTrack, callback) => {
     const [ error, setError ] = useState(null);
-    const [ subscriber, setSubcriber ] = useState(null);
-
-    const startWatching = async () => {
-        try {
-            await requestPermissionsAsync();
-            const sub = await watchPositionAsync({
-                accuracy: Accuracy.BestForNavigation,
-                timeInterval: 1000, //update once every second
-                distanceInterval: 10, //update once every 10 meters
-            }, 
-                callback
-            );
-
-            setSubcriber(sub);
-        } catch (err) {
-            setError(err);
-        }
-    };
 
     //Enables tracking when on TrackCreate screen and disables accordingly.
     useEffect (() => {
+        let subscriber;
+        const startWatching = async () => {
+            try {
+                await requestPermissionsAsync();
+                subscriber = await watchPositionAsync({
+                    accuracy: Accuracy.BestForNavigation,
+                    timeInterval: 1000, //update once every second
+                    distanceInterval: 10, //update once every 10 meters
+                }, 
+                    callback
+                );
+            } catch (err) {
+                setError(err);
+            }
+        };
+
         if( shouldTrack ) {
             startWatching();
         } else {
-            subscriber.remove();//stop the watching process entirely
-            setSubcriber(null);
+            if( subscriber ) {//in case of a subtle bug
+                subscriber.remove();//stop the watching process entirely
+            }
+            subscriber = null;
         }
+
+        return () => {
+            if( subscriber ) {
+                subscriber.remove();
+            }
+        };
     }, [shouldTrack, callback]);//compare to the last time the hook ran to determine if tracking is allowed
 
     return [ error ];
